97fc6a709a72cb2dfc6da25ea17b27c993611c8d
Alexander Bruy <alexander.bruy@gmail.com>
2018-01-16 14:50:23 +0200

    add translations (work in progress)

---
+++ i18n/processing_prepair_uk.ts
@@ -0,0 +1,137 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>PrepairProvider</name>
+    <message>
+        <location filename="../prepairProvider.py" line="61"/>
+        <source>Activate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prepairProvider.py" line="65"/>
+        <source>prepair executable</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prepairProvider.py" line="70"/>
+        <source>pprepair executable</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prepairProvider.py" line="75"/>
+        <source>Log commands output</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>pprepair</name>
+    <message>
+        <location filename="../pprepair.py" line="64"/>
+        <source>pprepair</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pprepair.py" line="67"/>
+        <source>Geometry tools</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pprepair.py" line="73"/>
+        <source>polygon,repair,broken,geometry</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pprepair.py" line="76"/>
+        <source>Automatic repair of planar partitions.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pprepair.py" line="85"/>
+        <source>Input layer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../pprepair.py" line="88"/>
+        <source>Repaired polygons</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>prepair</name>
+    <message>
+        <location filename="../prepair.py" line="77"/>
+        <source>prepair</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prepair.py" line="80"/>
+        <source>Geometry tools</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prepair.py" line="86"/>
+        <source>polygon,repair,broken,geometry</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prepair.py" line="89"/>
+        <source>Automatic repair of single polygons (according to the OGC Simple Features / ISO19107 rules) using a constrained triangulation.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prepair.py" line="100"/>
+        <source>Odd-even</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prepair.py" line="100"/>
+        <source>Point set topology</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prepair.py" line="102"/>
+        <source>Input layer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prepair.py" line="105"/>
+        <source>Repair paradigm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prepair.py" line="109"/>
+        <source>Remove sliver polygons</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prepair.py" line="114"/>
+        <source>Snap round input</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prepair.py" line="119"/>
+        <source>Process only invalid geometries</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prepair.py" line="123"/>
+        <source>Repaired polygons</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prepair.py" line="168"/>
+        <source>Feature {} is valid, skipping&#xe2;&#x80;&#xa6;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prepair.py" line="179"/>
+        <source>Feature {} not repaired</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../prepair.py" line="185"/>
+        <source>Empty geometry after repairing feature {}, skipping&#xe2;&#x80;&#xa6;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
